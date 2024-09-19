37aef1726f23c0a6415ccb2bc5103c3a673922ee
Doanh-Nguyen1207 <nthanhdoanh417@gmail.com>
2024-09-19 08:12:00 +0700

    write function and testing

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
-};
+const repeatString = function (string, num) {
+    const strOld = string
+    if (num < 0) return 'ERROR'
+    else if (num === 0) return ''
+    else if (num === 1) return string
+    else {
+        for (let i = 0; i < num - 1; i++) {
+            string += strOld
+        }
+        return string
+    }
+}
 
 // Do not edit below this line
-module.exports = repeatString;
+module.exports = repeatString
